c17e1b2eebcafeacac0657f745fc02a04b0c0058
crowlkats <crowlkats@gmail.com>
2020-04-05 07:04:34 +0200

    Yeet client to own repo

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
-export * as client from "./src/client/mod.ts";
 export * as gateway from "./src/gateway/mod.ts";
 export * as rest from "./src/rest/mod.ts";
 export * as structures from "./src/structures/mod.ts";
