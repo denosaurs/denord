8a07ed00db68d1289e5001c7e0b2d95d21da5dbc
crowlkats <13135287+crowlKats@users.noreply.github.com>
2020-08-09 23:47:07 +0200

    chore: bump std

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export {
   connectWebSocket,
   isWebSocketCloseEvent,
   WebSocket,
-} from "https://deno.land/std@0.63.0/ws/mod.ts";
-export { default as EventEmitter } from "https://deno.land/std@0.63.0/node/events.ts";
+} from "https://deno.land/std@0.64.0/ws/mod.ts";
+export { default as EventEmitter } from "https://deno.land/std@0.64.0/node/events.ts";
