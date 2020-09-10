7cdfe66404ee5a9549df8f04f0c6e751d8c80408
crowlkats <13135287+crowlKats@users.noreply.github.com>
2020-09-10 15:48:14 +0200

    feat(eventemitter): proper types for listeners object v2 & add asyncIterator

--- src/utils/EventEmitter.ts
+++ src/utils/EventEmitter.ts
@@ -1,5 +1,11 @@
-export default class EventEmitter<E extends Record<string, any[]>, K extends keyof E = keyof E, T extends E[K] = E[K]> {
-  listeners = {} as Record<K, ((...args: T) => void)[]>;
+export default class EventEmitter<E extends Record<string, any[]>> {
+  listeners: {
+    [K in keyof E]?: Array<{
+      once: boolean;
+      cb: (...args: E[K]) => void;
+    }>;
+  } = Object.create(null);
+  #writer: WritableStreamDefaultWriter<[keyof E, E[keyof E]]> | undefined;
 
   /**
    * Adds the listener function to the end of the listeners array for the event
@@ -8,24 +14,28 @@ export default class EventEmitter<E extends Record<string, any[]>, K extends key
    * listener will result in the listener being added, and called, multiple
    * times.
    */
-  on(eventName: K, listener: (...args: T) => void) {
-    if (!(eventName in this.listeners)) {
+  on<K extends keyof E>(eventName: K, listener: (...args: E[K]) => void) {
+    if (!this.listeners[eventName]) {
       this.listeners[eventName] = [];
     }
-    this.listeners[eventName].push(listener);
+    this.listeners[eventName]!.push({
+      once: false,
+      cb: listener,
+    });
   }
 
   /**
    * Adds a one-time listener function for the event named eventName. The next
    * time eventName is triggered, this listener is removed and then invoked.
    */
-  once(eventName: K, listener: (...args: T) => void) {
-    const wrapped = (...args: T): void => {
-      listener(...args);
-      this.off(eventName, listener);
-    };
-
-    this.on(eventName, wrapped);
+  once<K extends keyof E>(eventName: K, listener: (...args: E[K]) => void) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName]!.push({
+      once: true,
+      cb: listener,
+    });
   }
 
   /**
@@ -33,19 +43,17 @@ export default class EventEmitter<E extends Record<string, any[]>, K extends key
    * If no listener is passed, all listeners will be removed from eventName.
    * If no eventName is passed, all listeners will be removed from the EventEmitter.
    */
-  off(eventName?: K, listener?: (...args: T) => void) {
+  off<K extends keyof E>(eventName?: K, listener?: (...args: E[K]) => void) {
     if (eventName) {
-      if (eventName in this.listeners) {
-        if (listener) {
-          this.listeners[eventName] = this.listeners[eventName].filter(
-            (cb) => cb !== listener,
-          );
-        } else {
-          delete this.listeners[eventName];
-        }
+      if (listener) {
+        this.listeners[eventName] = this.listeners[eventName]?.filter(
+          ({ cb }) => cb !== listener,
+        );
+      } else {
+        delete this.listeners[eventName];
       }
     } else {
-      this.listeners = {} as Record<K, ((...args: T[]) => void)[]>;
+      this.listeners = Object.create(null);
     }
   }
 
@@ -54,11 +62,27 @@ export default class EventEmitter<E extends Record<string, any[]>, K extends key
    * eventName, in the order they were registered, passing the supplied
    * arguments to each.
    */
-  emit(eventName: K, ...args: T) {
-    if (eventName in this.listeners) {
-      for (const listener of this.listeners[eventName]) {
-        listener(...args);
+  emit<K extends keyof E>(eventName: K, ...args: E[K]) {
+    const listeners = this.listeners[eventName]?.slice() ?? [];
+    for (const { cb, once } of listeners) {
+      cb(...args);
+
+      if (once) {
+        this.off(eventName, cb);
       }
     }
+
+    return this.#writer?.write([eventName, args]);
+  }
+
+  async *[Symbol.asyncIterator](): AsyncIterableIterator<
+    { [K in keyof E]: [K, E[K]] }[keyof E]
+  > {
+    const { readable, writable } = new TransformStream<
+      [keyof E, E[keyof E]],
+      [keyof E, E[keyof E]]
+    >();
+    this.#writer = writable.getWriter();
+    yield* readable.getIterator();
   }
 }
