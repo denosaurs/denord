dfd5457e15710f1c9b116695f603f08b25f2cf0b
Qu4k <pippo.reds@gmail.com>
2020-09-14 15:21:12 +0200

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -2,8 +2,9 @@ export {
   connectWebSocket,
   isWebSocketCloseEvent,
   WebSocket,
-} from "https://deno.land/std@0.68.0/ws/mod.ts";
+  // depsbot-ignore
+} from "https://deno.land/std@0.69.0/ws/mod.ts";
 
-export { equal } from "https://deno.land/std@0.68.0/testing/asserts.ts";
+export { equal } from "https://deno.land/std@0.69.0/testing/asserts.ts";
 
 export { default as EventEmitter } from "https://deno.land/x/event@0.1.0/mod.ts";
